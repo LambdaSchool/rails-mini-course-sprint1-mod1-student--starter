f65c390a02af9b027f20a1a014f1f36ab003a5fb
luis <mrluismartinezzz@gmail.com>
2019-08-12 12:30:35 -0700

    comment results

--- language_screening.rb
+++ language_screening.rb
@@ -5,3 +5,16 @@ def language_screening(people, language)
   end
   result
 end
+
+# language_screening(candidates, "Python") => 
+
+# [{:first_name=>"Sara", :last_name=>"Smith", :level=>:mid_level, :age=>26, :occupation=>"Software Developer", :employed=>true, :current_salary=>80000.0, :languages=>["Ruby", "Python"]}, {:first_name=>"Jason", :last_name=>"Gray", :level=>:entry_level, :age=>23, :occupation=>"Student", :employed=>false, :current_salary=>0.0, :languages=>["C++", "Python"]}]
+
+
+# language_screening(candidates, "GO") =>
+
+# [{:first_name=>"Jane", :last_name=>"Doe", :level=>:senior_level, :age=>30, :occupation=>"Software Developer", :employed=>true, :current_salary=>125000.0, :languages=>["Ruby", "JavaScript", "Go"]}]
+
+# language_screening(candidates, "javascript") =>
+
+# [{:first_name=>"Jane", :last_name=>"Doe", :level=>:senior_level, :age=>30, :occupation=>"Software Developer", :employed=>true, :current_salary=>125000.0, :languages=>["Ruby", "JavaScript", "Go"]}]
