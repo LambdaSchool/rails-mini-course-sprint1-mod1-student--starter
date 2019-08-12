663c998707a01f573ebeb13abce1bd6c0a37f7ee
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-12 13:40:14 -0400

    made the jane hash

---
+++ candidates.rb
@@ -0,0 +1,11 @@
+jane = {
+    first_name: "Jane",
+    last_name: "Doe",
+    level: "senior_level",
+    age: 30,
+    occupation: "Software Developer",
+    employed: true,
+    current_salary: 125,000.00
+    languages: ["Ruby", "JavaScript", "Go"]
+}
+
