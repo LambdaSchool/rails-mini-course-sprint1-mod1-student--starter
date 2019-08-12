9e5695d6ee34618780727c7b80580d271c9674e4
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-12 15:54:41 -0400

    changing jane hash to fix errors

--- candidates.rb
+++ candidates.rb
@@ -1,11 +1,20 @@
-jane = {
-    first_name: "Jane",
-    last_name: "Doe",
-    level: "senior_level",
-    age: 30,
-    occupation: "Software Developer",
-    employed: true,
-    current_salary: 125,000.00
-    languages: ["Ruby", "JavaScript", "Go"]
-}
+first_name: "Jane"
+last_name: "Doe"
+level: "senior_level"
+age: 30
+occupation: "Software Developer"
+employed: true
+current_salary: 125,000.00
+languages: []
+languages: "Ruby", "JavaScript", "Go"
 
+jane = {
+    first_name: first_name,
+    last_name: last_name,
+    level: level,
+    age: age,
+    occupation: occupation,
+    employed: employed,
+    current_salary: current_salary,
+    languages: languages
+}
