8df156f774e2449a1164e5fac100601cef506cd2
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-12 15:58:45 -0400

    got jane hash working with no errors

--- candidates.rb
+++ candidates.rb
@@ -1,20 +1,10 @@
-first_name: "Jane"
-last_name: "Doe"
-level: "senior_level"
-age: 30
-occupation: "Software Developer"
-employed: true
-current_salary: 125,000.00
-languages: []
-languages: "Ruby", "JavaScript", "Go"
-
-jane = {
-    first_name: first_name,
-    last_name: last_name,
-    level: level,
-    age: age,
-    occupation: occupation,
-    employed: employed,
-    current_salary: current_salary,
-    languages: languages
-}
+jane = {}
+jane[:first_name] = "Jane"
+jane[:last_name] = "Doe"
+jane[:level] = "senior_level"
+jane[:age] = 30
+jane[:occupation] = "Software Developer"
+jane[:employed] = true
+jane[:current_salary] = 125,000.00
+jane[:languages] = []
+jane[:languages] = "Ruby", "JavaScript", "Go"
