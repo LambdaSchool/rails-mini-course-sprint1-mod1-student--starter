c85f39c1ebc40879382d10b0c5a433122f8196f9
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-03-01 18:36:38 -0600

    Completed step 2

--- candidates.rb
+++ candidates.rb
@@ -6,3 +6,11 @@ jason = { :first_name=>"Jason", :last_name=>"Gray", :level=>"entry_level", :age=
 
 
 candidates = [jane, sara, jason]
+
+def average_age(people)
+    ages = people.map { |person| person[:age]}
+    ages_sum = ages.reduce(0) { |sum, age| sum + age }
+    average = ages_sum/3.0
+end
+
+puts average_age(candidates)
