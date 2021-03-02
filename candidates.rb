3c79e6c423565970828af0d3e6dcd583e00b4cb5
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-03-01 19:17:00 -0600

    Added candidates.rb

--- candidates.rb
+++ candidates.rb
@@ -1,16 +1,13 @@
-jane = { :first_name=>"Jane", :last_name=>"Doe", :level=>"senior_level", :age=>30, :occupation=>"Software Developer", :employed=>true, :current_salary=>125000.00, :languages=>["Ruby", "Python", "Go"]}
-
-sara = { :first_name=>"Sara", :last_name=>"Smith", :level=>"mid_level", :age=>26, :occupation=>"Software Developer", :employed=>true, :current_salary=>80000.00, :languages=>["Ruby, Python"] }
-
-jason = { :first_name=>"Jason", :last_name=>"Gray", :level=>"entry_level", :age=>23, :occupation=>"Student", :employed=> false, :current_salary=>0.00, :languages=>["C++", "Python"] }
-
-
-candidates = [jane, sara, jason]
-
-
-
-
-
+def language_screening(people, language)
+    qualified_people = []
+    people.each do |person|
+        candidate_languages = person[:languages].map { |language| language.downcase }
+        if candidate_languages.include?(language.downcase)
+            qualified_people.push(person[:first_name])    
+        end
+    end
+    return qualified_people
+end
 
 
 
